698123f60e48cbf5d6fb719550e982ba0d436dfe
Almas Rakyshbaev <kasper10.0711@gmail.com>
2023-03-16 09:17:02 +0600

    изменено название файла view_tasks.py > views_tasks.py

--- source/issue_tracker/views/__init__.py
+++ source/issue_tracker/views/__init__.py
@@ -1,1 +1,1 @@
-from .view_tasks import IndexView, CreateTask, DetailTaskView, DeleteTaskView, UpdateTaskView
+from .views_tasks import IndexView, CreateTask, DetailTaskView, DeleteTaskView, UpdateTaskView
