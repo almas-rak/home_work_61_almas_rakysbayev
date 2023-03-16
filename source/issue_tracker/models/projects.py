44fd5645141de2e56c9cedeba2a2f40da8a6cb3f
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 22:11:36 +0600

    изменил названия полей в поле deleted_at добавил blank=True

--- source/issue_tracker/models/projects.py
+++ source/issue_tracker/models/projects.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 
 
 class Project(models.Model):
-    created_at = models.DateField(verbose_name='Дата и время создания')
-    completed_at = models.DateField(null=True, blank=True, verbose_name='Дата и время окончания')
+    created_at = models.DateField(verbose_name='Дата создания')
+    completed_at = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
     name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(verbose_name='описание')
     is_deleted = models.BooleanField(default=False, verbose_name='удалено')
-    deleted_at = models.DateTimeField(null=True, verbose_name='Дата и время удаления')
+    deleted_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время удаления')
 
     def delete(self, using=None, keep_parents=False):
         self.is_deleted = True
