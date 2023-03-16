9589726437bfd45f47b8af4edd47c85eaafc8e89
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 23:00:12 +0600

    добавлено представление создания проекта

--- source/issue_tracker/views/views_projects.py
+++ source/issue_tracker/views/views_projects.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from issue_tracker.forms import ProjectForm
 from issue_tracker.models import Project, Task
 
 
@@ -25,3 +27,13 @@ class DetailProjectView(DetailView):
         tasks = Task.objects.filter(project=project, is_deleted=False)
         context['tasks'] = tasks
         return context
+
+
+class CreateProjectView(CreateView):
+    template_name = 'projects_templates/create_project.html'
+    model = Project
+    form_class = ProjectForm
+
+    def get_success_url(self):
+        return reverse('detail_project', kwargs={'pk': self.object.pk})
+
