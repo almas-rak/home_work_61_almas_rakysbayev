5b07a4b94dfe7d225927d9c630fa8886e42ebac0
Almas Rakyshbaev <kasper10.0711@gmail.com>
2023-03-16 08:14:54 +0600

    добавлена ссылка на обнавление задачи

--- source/issue_tracker/urls.py
+++ source/issue_tracker/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from issue_tracker.views import IndexView, CreateTask, DetailTaskView, DeleteTaskView
+from issue_tracker.views import IndexView, CreateTask, DetailTaskView, DeleteTaskView, UpdateTaskView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
     path("create/task", CreateTask.as_view(), name='create_task'),
     path("detail/task/<int:pk>", DetailTaskView.as_view(), name='detail_task'),
     path("delete/task/<int:pk>", DeleteTaskView.as_view(), name='delete_task'),
+    path("update/task/<int:pk>", UpdateTaskView.as_view(), name='update_task'),
 
 ]
