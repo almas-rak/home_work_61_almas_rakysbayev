747fb10908f73573ac8ecd019b397d41f6b48457
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 23:02:58 +0600

    добавлено представление создания проекта

--- source/issue_tracker/views/__init__.py
+++ source/issue_tracker/views/__init__.py
@@ -1,2 +1,2 @@
 from .views_tasks import IndexView, CreateTask, DetailTaskView, DeleteTaskView, UpdateTaskView
-from .views_projects import ListProjectView, DetailProjectView
+from .views_projects import ListProjectView, DetailProjectView, CreateProjectView
