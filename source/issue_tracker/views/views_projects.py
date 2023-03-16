af11202fd0c5f8f6d6eaff9a30edd87fd3980b64
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 20:34:33 +0600

    добавленно представление списка проектов

--- source/issue_tracker/views/views_projects.py
+++ source/issue_tracker/views/views_projects.py
@@ -1,5 +1,14 @@
 from django.views.generic import ListView
 
+from issue_tracker.models import Project
+
 
 class ListProjectView(ListView):
-    pass
+    template_name = 'projects_templates/list_projects.html'
+    model = Project
+    context_object_name = 'projects'
+    ordering = 'created_at'
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_deleted=True)
+        return queryset
