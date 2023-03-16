348087cea93e50399572018e1dcc61f564271df8
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 20:35:57 +0600

    добавленно мягкое удаление

--- source/issue_tracker/models/projects.py
+++ source/issue_tracker/models/projects.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Project(models.Model):
@@ -6,6 +7,17 @@ class Project(models.Model):
     completed_at = models.DateField(null=True, blank=True, verbose_name='Дата и время окончания')
     name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(verbose_name='описание')
+    is_deleted = models.BooleanField(default=False, verbose_name='удалено')
+    deleted_at = models.DateTimeField(null=True, verbose_name='Дата и время удаления')
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
