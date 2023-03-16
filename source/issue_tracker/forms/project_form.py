0c71c4917ce91d4d3ea14251744afdaee25c885f
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 23:01:37 +0600

    добавлены виджеты для дат

--- source/issue_tracker/forms/project_form.py
+++ source/issue_tracker/forms/project_form.py
@@ -4,6 +4,9 @@ from issue_tracker.models import Project
 
 
 class ProjectForm(forms.ModelForm):
+    created_at = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    completed_at = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Project
         fields = ('name', 'description', 'created_at', 'completed_at')
