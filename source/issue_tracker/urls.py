b0f97f7751dc504e55d0db36933eb800a7227b08
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-16 07:02:31 +0600

    добавлена ссылки на создание задачи и детального представления задачи

--- source/issue_tracker/urls.py
+++ source/issue_tracker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from issue_tracker.views import IndexView
+from issue_tracker.views import IndexView, CreateTask, DetailTaskView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
+    path("create/task", CreateTask.as_view(), name='create_task'),
+    path("detail/task/<int:pk>", DetailTaskView.as_view(), name='detail_task'),
 ]
